1a7d0cdeefe5cc6981b2c1e2f667e8685c5bd278
paldana <paul.aldana23@gmail.com>
2026-08-05 15:09:25 -0700

    Using SQLite3 instead of PostgreSQL

--- database.py
+++ database.py
@@ -2,12 +2,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase, declarative_base
 
 ## SQLite3 
-# SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
-# engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
+SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
 
 ## PostgreSQL
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:test1234!@localhost/TodoApplicationDatabase'
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+# SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:test1234!@localhost/TodoApplicationDatabase'
+# engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 # create a sessionmaker for the database
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
